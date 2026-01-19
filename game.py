64460f22756a48aee7f287bf11428e101364260b
erzaakbart <gamedaungedhang@gmail.com>
2026-01-19 06:31:12 +0000

    tugas tik.v2

---
+++ game.py
@@ -0,0 +1,9 @@
+secret_number = 777
+guess_number = int(input("Masukkan Tebak Angka: "))
+
+while guess_number != secret_number:
+    print("Tebakan salah, silahkan coba lagi")
+    guess_number = int(input("Masukkan Tebak Angka: "))
+
+print("Selamat!! Tebakan anda benar")
+print("Kode ini saya buat di codespace")
